a4faaef5f6eb821d8c53ec25e0268b62daa784ad
TLEE337 <tyler_mj_lee@berkeley.edu>
2026-03-04 19:28:14 -0800

    rough draft heat sensor

---
+++ 2526testing/heatsensor.py
@@ -0,0 +1,11 @@
+Temp_on = 5 #If heat senors below 5C then turn on heater
+Temp_off = 0 
+heater_on  = False
+
+
+
+def check_heater()
+  temp = check_temp #set the temperature equal to the temperature you get when you check
+  
+  if (heat_senor == Temp_on):
+  heater_on  = True

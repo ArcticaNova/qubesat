fa001a3d89574ca3461141a40784081d9652a2b7
benjamin.woo <benjamin.woo@berkeley.edu>
2026-04-01 19:38:05 -0700

    more burnwire progress

--- 26NewCode/BurnWire.py
+++ 26NewCode/BurnWire.py
@@ -4,13 +4,22 @@ import pycubed
 import digitalio
 
 class BurnWire() :
-    wire1 = 1 #for each of the burn wires
-    wire2 = 2
+    wire1 = "1" #for each of the burn wires
+    wire2 = "2"
+    freq = 0
+    burn_num = ""
+    duty_cycle = 0
 
     def __init__(self) :
         self.isBurning = False #turn to not isBurning once started and then not isBurning after the necessary time runs (30 min i think)
-        pycubed.cubesat._relayA.drive_mode=digitalio.DriveMode.PUSH_PULL
+        pycubed.cubesat._relayA.drive_mode = digitalio.DriveMode.PUSH_PULL
         pycubed.cubesat._relayA.value = True
+        
+        if self.burn_num == "1" :
+            burnwire = pycubed.cubesat.pwmio.PWMOut(self.wire1, self.freq, self.duty_cycle)
+        elif self.burn_num == "2" :
+            burnwire = pycubed.cubesat.pwmio.PWMOut(self.wire2, self.freq, self.duty_cycle)
 
-    def burn(self) :
-        if self.isBurning :
+
+    def cubesatBurn(self, burn_num, dutycycle, freq) :
+        
